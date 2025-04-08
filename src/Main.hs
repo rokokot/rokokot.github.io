daa846f2e296b36340d32bf3c85a99b8e94ee8d5
Robin Kokot <robin.edu.hr@gmail.com>
2025-04-08 23:29:39 +0200

    update main

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-
 module Main where
 
 import           Data.Monoid ()
@@ -8,61 +7,69 @@ import           Hakyll
 
 main :: IO ()
 main = hakyll $ do
-    match ("images/*" .||. "files/*" .||. "static/*/*") $ do
-        route   idRoute
+    match ("site/images/*" .||. "site/files/*" .||. "site/static/*/*") $ do
+        route   (gsubRoute "site/" (const ""))
         compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
+    match "site/css/*" $ do
+        route   (gsubRoute "site/" (const ""))
         compile compressCssCompiler
 
-    match "templates/*" $ compile templateBodyCompiler
+    match "site/js/*" $ do
+        route   (gsubRoute "site/" (const ""))
+        compile copyFileCompiler
 
-    match (fromList ["index.md", "about.md", "research.md", "contact.md"]) $ do
-        route   $ setExtension "html"
+    match "site/templates/*" $ compile templateBodyCompiler
+
+    -- Create metadata versions of project pages
+    match ("site/projects/*" .&&. complement "site/projects/TEMPLATE.md") $ version "meta" $ do
+        route $ gsubRoute "site/" (const "") `composeRoutes` setExtension "html"
+        compile getResourceBody
+
+    -- Regular pages
+    match (fromList ["site/index.md", "site/about.md", "site/research.md", "site/contact.md"]) $ do
+        route   $ gsubRoute "site/" (const "") `composeRoutes` setExtension "html"
         compile $ do
-            projects <- loadAll "projects/*"
+            projects <- loadAll ("site/projects/*" .&&. hasVersion "meta")
             let pageCtx = 
                     listField "projects" siteCtx (return projects) `mappend`
                     siteCtx
             pandocCompiler
-                >>= loadAndApplyTemplate "templates/default.html" pageCtx
+                >>= loadAndApplyTemplate "site/templates/default.html" pageCtx
                 >>= relativizeUrls
 
-    match "projects/*" $ do
-        route $ setExtension "html"
+    -- Project pages
+    match ("site/projects/*" .&&. complement "site/projects/TEMPLATE.md") $ do
+        route $ gsubRoute "site/" (const "") `composeRoutes` setExtension "html"
         compile $ do
-            projects <- loadAll "projects/*"
+            projects <- loadAll ("site/projects/*" .&&. hasVersion "meta")
             let projCtx = 
                     listField "projects" siteCtx (return projects) `mappend`
                     siteCtx
             pandocCompiler
-                >>= loadAndApplyTemplate "templates/project.html" projCtx
-                >>= loadAndApplyTemplate "templates/default.html" projCtx
+                >>= loadAndApplyTemplate "site/templates/project.html" projCtx
+                >>= loadAndApplyTemplate "site/templates/default.html" projCtx
                 >>= relativizeUrls
-                
-    match "js/*" $ do
-        route idRoute
-        compile copyFileCompiler
 
-
+    -- Projects index
     create ["projects.html"] $ do
         route idRoute
         compile $ do
-            projects <- loadAll "projects/*"
+            projects <- loadAll ("site/projects/*" .&&. hasVersion "meta")
             let projectsCtx =
                     listField "projects" siteCtx (return projects) `mappend`
                     constField "title" "Projects" `mappend`
                     siteCtx
             makeItem ""
-                >>= loadAndApplyTemplate "templates/projects-list.html" projectsCtx
-                >>= loadAndApplyTemplate "templates/default.html" projectsCtx
+                >>= loadAndApplyTemplate "site/templates/projects-list.html" projectsCtx
+                >>= loadAndApplyTemplate "site/templates/default.html" projectsCtx
                 >>= relativizeUrls
 
 siteCtx :: Context String
 siteCtx =
     constField "site_name" "Robin Kokot" `mappend`
     constField "university" "KU Leuven" `mappend`
+    constField "department" "Faculty of Engineering Science" `mappend`
     constField "email" "robin.edu.hr@gmail.edu" `mappend`
     constField "github_username" "rokokot" `mappend`
     defaultContext
